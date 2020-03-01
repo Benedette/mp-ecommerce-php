317dc87996e10dd6d98d4732a862d237aa95230b
Nicolas G Fernandez <nicolas.fernandez@mercadolibre.com>
2020-03-01 15:41:36 -0300

    test

--- api/notifications.php
+++ api/notifications.php
@@ -5,8 +5,11 @@ session_start();
 if (isset($_POST["id"], $_POST["topic"])) {
     http_response_code(200);
 
-    print_r($_GET["id"]);
-    print_r($_GET["topic"]);
+    print_r($_POST["id"]);
+    print_r($_POST["topic"]);
+
+    var_dump($_POST["id"]);
+    var_dump($_POST["topic"]);
     return;
 
 
