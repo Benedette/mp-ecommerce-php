dab37dd4fd527d345ff161b510869481862d11f7
Nicolas G Fernandez <nicolas.fernandez@mercadolibre.com>
2020-03-01 16:08:03 -0300

    test

--- api/notifications.php
+++ api/notifications.php
@@ -6,11 +6,13 @@ http_response_code(200);
 
 // Takes raw data from the request
 $json = file_get_contents('php://input');
+$get = json_encode($_GET);
 
 // Converts it into a PHP object
 $data = json_decode($json);
 
 //print_r($data);
-error_log($json)
+error_log($json);
+error_log($get);
 
 ?>
