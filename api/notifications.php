56befd57143245b80d721a80fefa880bfd79cff3
Nicolas G Fernandez <nicolas.fernandez@mercadolibre.com>
2020-03-01 17:57:56 -0300

    test

--- api/notifications.php
+++ api/notifications.php
@@ -4,15 +4,17 @@ http_response_code(200);
 
 // Takes raw data from the request
 $json = file_get_contents('php://input');
-
 error_log($json);
+
+if ($topic != "payments") {
+    return;
+}
 
 // Converts it into a PHP object
 $data = json_decode($json, true);
 error_log($data['topic']);
 error_log($_GET['id']);
 $topic = $data['topic'];
-$id = $_GET['id'];
 $session_id = $_GET['session_id'];
 
 session_id($session_id);
@@ -23,6 +25,7 @@ session_start();
 //$resource = $data['resource'];
 
 if ($topic == "merchant_order") {
+    $id = $_GET['id'];    
     $url_mo = 'https://api.mercadopago.com/merchant_orders/'.$id.'?access_token=APP_USR-8784583960835302-012920-171e94e7f45cc4e31e3cbcd15fb591ef-520255910';
     $ch = curl_init($url_mo);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
