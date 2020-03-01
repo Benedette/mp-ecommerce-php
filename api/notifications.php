5a35e65a95f3c26e0c2e74ac136a1d85df11a664
Nicolas G Fernandez <nicolas.fernandez@mercadolibre.com>
2020-03-01 16:50:04 -0300

    test

--- api/notifications.php
+++ api/notifications.php
@@ -20,7 +20,7 @@ $id = $_GET['id'];
 //$resource = $data['resource'];
 
 if ($topic == "merchant_order") {
-    $url_mo = 'https://api.mercadopago.com/merchant_orders/'.$id;
+    $url_mo = 'https://api.mercadopago.com/merchant_orders/'.$id.'?access_token=APP_USR-8784583960835302-012920-171e94e7f45cc4e31e3cbcd15fb591ef-520255910';
     $ch = curl_init($url_mo);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
