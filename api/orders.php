3818d56c27d3414824d5a25523691eca0952aec8
Nicolas G Fernandez <nicolas.fernandez@mercadolibre.com>
2020-03-01 15:21:29 -0300

    test

--- api/orders.php
+++ api/orders.php
@@ -5,8 +5,8 @@ http_response_code(200);
 $mp_endpoint = 'https://api.mercadopago.com/mpmobile/instore/qr/520255910/pythontest?access_token=APP_USR-8784583960835302-012920-171e94e7f45cc4e31e3cbcd15fb591ef-520255910';
 
 $title = $_POST['title'];
-$quantity = $_POST['quantity'];
-$unit_price = $_POST['unit_price'];
+$quantity = intval($_POST['quantity']);
+$unit_price = intval($_POST['unit_price']);
 $currency_id = 'ARS';
 
 $fecha = date_create();
@@ -19,9 +19,6 @@ $items = array('title' => $title, 'currency_id' => $currency_id ,'quantity' => $
 
 $json = array('external_reference' => $external_reference, 'notification_url' => $notification_url, 'items' => $items);
 
-echo $json;
-var_dump($json);
-
 $ch = curl_init($mp_endpoint);
 
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($json));
@@ -29,4 +26,5 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 curl_close($ch);
+var_dump($result);
 ?>
