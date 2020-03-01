d19f6b1883219a12bc51cfda367691b5cc5203b9
Nicolas G Fernandez <nicolas.fernandez@mercadolibre.com>
2020-03-01 18:11:59 -0300

    test

--- api/status.php
+++ api/status.php
@@ -4,4 +4,10 @@ session_start();
 error_log("Session: ".session_id());
 error_log(json_encode($_SESSION));
 
+$external_reference = $_GET['external_reference'];
+$status = $_SESSION['db'][$external_reference];
+
+header('Content-type: application/json');
+echo json_encode(["status" => $status]);
+
 ?>
