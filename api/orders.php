92168efeb2ac7b634b1e88ef8f2d065add568c5c
Nicolas G Fernandez <nicolas.fernandez@mercadolibre.com>
2020-03-01 18:34:32 -0300

    test

--- api/orders.php
+++ api/orders.php
@@ -30,6 +30,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 curl_close($ch);
-var_dump($result);
+
+echo json_encode($result);
 
 ?>
