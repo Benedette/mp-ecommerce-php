154356b52e016898093d4a5f6549c3eff06bbf2c
Benedette <rodrigo.benedette@mercadolivre.com>
2020-04-20 18:58:17 -0300

    Update delete-order.php

--- api/delete-order.php
+++ api/delete-order.php
@@ -2,7 +2,7 @@
 
 http_response_code(200);
 
-$mp_endpoint = 'https://api.mercadopago.com/mpmobile/instore/qr/520255910/pythontest?access_token=APP_USR-8784583960835302-012920-171e94e7f45cc4e31e3cbcd15fb591ef-520255910';
+$mp_endpoint = 'https://api.mercadopago.com/mpmobile/instore/qr/519628608/caixa01loja01?access_token=APP_USR-7092-012814-71dc5f92829a009096d9bf914a4900d0-519628608';
 
 $ch = curl_init($mp_endpoint);
 
