061a991c953259fd9ac39c7be126a77cf2cc9145
Benedette <rodrigo.benedette@mercadolivre.com>
2020-04-20 18:06:06 -0300

    Update orders.php

--- api/orders.php
+++ api/orders.php
@@ -17,7 +17,7 @@ $fecha = date_create();
 $time_stamp = date_timestamp_get($fecha);
 
 $external_reference = 'niclas-' . $time_stamp;
-$notification_url = 'https://niclas-mp-commerce-php.herokuapp.com/api/notifications.php?session_id='.session_id();
+$notification_url = 'https://benedette-mp-commerce-php.herokuapp.com/api/notifications.php?session_id='.session_id();
 
 $items = array('title' => $title, 'currency_id' => $currency_id ,'quantity' => $quantity, 'unit_price' => $unit_price);
 
